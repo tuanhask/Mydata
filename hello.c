ee1f0d6efdca43e14c6d49a19209b468dc1635c1
Tuan Tran <35459431+tuanhask@users.noreply.github.com>
2019-12-17 14:32:36 +0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,1 +1,57 @@
-Hello world !!!
+Amazon Web Service
+- Amazon Athena - Truy vấn dữ liệu trong S3 bằng sql
+- Amazon CloudSearch - Dịch vụ tìm kiếm được quản lý
+- Amazon Elasticsearch Service - Chạy và mở rộng cụm máy chủ service
+- Amazon ERM - Hadoop Framework trong môi trường dữ liệu
+- Amazon Kinesis - Thao tác với dữ liệu phát trực tuyến trong thời gian thực
+- Amazon Managed Streaming for Apache Kafka - dịch vụ AK được quản lý toàn phần
+- AWS RDS : Là một dịch vụ được quản lý giúp bạn dễ dàng thiết lập,
+vận hành và thay đổi quy mô cơ sở dữ liệu quan hệ trên
+đám mây.
+- EC2 : Amazon Elastic Compute Cloud (AWS EC2) là 1 cơ sở
+hạ tầng điện toán đám mây được cung cấp bởi AWS
+giúp cung cấp tài nguyên máy ảo hóa theo yêu cầu.
+- S3 : Amazon Simple Storage Service (Amazon S3) là
+một dịch vụ lưu trữ đối tượng cung cấp khả năng
+thay đổi theo quy mô, tính khả dụng của dữ liệu,
+bảo mật và hiệu năng hàng đầu trong lĩnh vực.
+- ECS Amazon Elastic Container Service : quản lý các container nhanh và khả năng mở rộng, dễ dàng chạy và quản lý các thùng chứa docker
+- Fargate - chạy các container mà không cần quan tâm đến quản trị server
+- ECR Amazon Elastic Container Registry : Lưu trữ image của docker đã chạy ở máy local
+- ALB Application Load Balancer : Phân phối kết quả lưu lượng truy cập đến trên một nhóm backend servers
+- VPC : Tạo môi trượng mạng riêng ảo 
+- AMZ Aurora : Amazon Aurora (Aurora) là một cơ sở dữ liệu quan hệ được cung cấp bởi Amazon, được quản lý đầy đủ, tương thích với MySQL và PostgreSQL và được xây dựng cho cloud, kết hợp hiệu suất và tính khả dụng của các cơ sở dữ liệu thương mại cao cấp với tính đơn giản và hiệu quả về chi phí của các cơ sở dữ liệu mã nguồn mở.
+Hiệu năng cao và khả năng mở rộng.
+* Tính khả dụng và độ bền cao.
+* Tương thích MySQL và PostgreSQL.
+* Quản lý hoàn toàn.
+- Amazon QuickSight là một dịch vụ phân tích kinh doanh mà bạn có thể sử dụng để xây dựng trực quan hóa, thực hiện phân tích đặc biệt và nhận thông tin kinh doanh từ dữ liệu của bạn
+Ưu điểm của Amazon QuickSight
+Chỉ phải trả tiền cho những gì mình sử dụng.
+Mở rộng cho tất cả người dùng.
+Kết hợp phân tích dữ liệu trong ứng dụng.
+Xây dựng các giải pháp BI hoàn chỉnh.
+
+Server failure - Các nguyên nhân gây ra : do thiên tai, thảm hoạ, phần cứng máy tínhđã tới thời kì thay thế, do con người, lỗi phần mềm
+Spike traffic - Spike traffic là sự gia tăng đột biến của request vào server Do các đợt sale off, campaign, quảng cáo hoặc đăng kí tín chỉ,ddos
+Auto scaling group Tự động nhân rộng để đảm bảo rằng các instance Amazon EC2 đủ để chạy các ứng dụng của bạn. Đảm bảo số lượng instance EC2 tối thiểu.
+Elastic load balancer- Cân bằng tải 
+AWS Lambda là một mô hình event-driven, nền tảng Serverless trên AWS này sẽ thực thi mã của bạn rồi phản hồi lại các sự kiệnchúng tùy theo . Nó sẽ quản lý cơ sở hạ tầng cơ bản và tăng hoặc giảm lượng yêu cầu xử lý sự kiện của ứng dụng.
+
+Vitur
+GOOGLE :
+- Kubernetes Engine : Là sản phẩm dành cho việc deploy các ứng dụng đã được container hoá. Nó mang đến những cải tiến mới nhất về năng suất của nhà phát triển, tính linh hoạt của mã nguồn mở
+- App Engine : Nền tảng giúp bạn phát triển ứng dụng một cách nhanh chóng, dễ dàng mà không cần phải lo về hạ tầng, với khả năng tự động đáp ứng lưu lượng truy cập không giới hạn.
+- Cloud Functions : Dịch vụ hỗ trợ thực hiện các tác vụ hướng sự kiện đơn giản như tạo email hay phản hồi hành động nào đó khi ứng dụng của bạn đang hoạt động. Tự động đồng bộ và sử dụng dễ dàng.
+- BigQuery : Data warehouse có khả năng lưu trữ tới hàng trăm TB và tích hợp đầy đủ các tính năng giúp cho việc phân tích dữ liệu dễ dàng hơn.
+- Cloud Pub/Sub : Dịch vụ hỗ trợ giao tiếp giữa các ứng dụng trên GCP, thông qua đồng bộ việc gửi - nhận các message giữa các ứng dụng khác nhau, giúp cho quá trình phát triển ứng dụng của bạn nhanh chóng, rõ ràng và hiệu quả hơn.
+- Cloud Dataflow : Dịch vụ giúp xây dựng các luồng xử lí dữ liệu một cách dễ dàng hơn, hỗ trợ cả các luồng xử lí liên tục và xử lí theo lô, với giao diện người dùng thân thiện
+- Cloud Dataprep : Dịch vụ khai phá và xử lý dữ liệu thô thành dữ liệu "sạch", để dùng cho phân tích, báo cáo hoặc máy học (Machine Learning).
+- Cloud Bigtable : Dịch vụ cơ sở dữ liệu NoSQL có khả năng chịu tải cao, quản lý dễ dàng, và có thể lưu trữ dữ liệu quy mô petabyte.
+- Cloud Datastore : Hệ cơ sở dữ liệu NoSQL có hỗ trợ các giao thức như SQL, có khả năng tự mở rộng cao, đáp ứng được cho các ứng dụng đến 10 tỉ người sử dụng.
+- Cloud SQL : Dịch vụ quản lý  MySQL, PostgreSQL trên Google Cloud Platform.
+- Cloud Spanner  Là dịch vụ quản lý đầy đủ, có thể mở rộng theo chiều ngang (horizontal) dành cho cơ sở dữ liệu quan hệ ở mức độ  khu vực và toàn cầu.
+- Cloud Machine Learning Engine Dịch vụ quản lý, tạo các mô hình máy học (model Machine Learning) với TensorFlow, làm việc được trên những tập dự liệu cực lớn.
+- Cloud Speech-to-Text : API chuyển đổi lời nói thành văn bản được cung cấp bởi máy học machine learning. Nó nhận ra khoảng 120 ngôn ngữ và biến thể để hỗ trợ người dùng toàn cầu.
+- Cloud Text-to-Speech : API chuyển đổi văn bản thành lời nói.
+- Vision AI :  Là hệ thống nhận dạng vật thể, phân loại hình ảnh, phát hiện cảm xúc, hiểu được văn bản, dựa trên các model đã được huấn luyện.
